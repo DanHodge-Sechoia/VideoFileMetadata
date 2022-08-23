ee368f20e1f95d8a3293be1d16de80b3ccdb79c5
Dan Hodge <d.hodge@sechoia.com>
2022-08-23 10:42:45 +0100

    Builds and runs but doesn't really do much.

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <gst/gst.h>
 #include <gst/mpegts/mpegts.h>
 
-#define PIPELINE_STR "videotestsrc num-buffers=100 ! x264enc ! queue ! mpegtsmux name=mux ! fakesink"
+#define PIPELINE_STR "videotestsrc num-buffers=1000 ! x264enc ! queue ! mpegtsmux name=mux ! fakesink"
 
 static void
 _on_bus_message (GstBus * bus, GstMessage * message, GMainLoop * mainloop)
 {
   switch (GST_MESSAGE_TYPE (message)) {
     case GST_MESSAGE_ERROR:
+      g_print ("ERROR\n");
     case GST_MESSAGE_EOS:
+      g_print ("ENDING LOOP\n");
       g_main_loop_quit (mainloop);
       break;
     default:
@@ -51,14 +53,17 @@ advertise_service (GstElement * mux)
 int
 main (int argc, char **argv)
 {
+  g_print ("main\n");
   GstElement *pipeline = NULL;
   GError *error = NULL;
   GstBus *bus;
   GMainLoop *mainloop;
   GstElement *mux;
 
+  g_print ("gst_init\n");
   gst_init (&argc, &argv);
 
+  g_print ("gst_parse_launch\n");
   pipeline = gst_parse_launch (PIPELINE_STR, &error);
   if (error) {
     g_print ("pipeline could not be constructed: %s\n", error->message);
@@ -66,25 +71,32 @@ main (int argc, char **argv)
     return 1;
   }
 
+  g_print ("g_main_loop_new\n");
   mainloop = g_main_loop_new (NULL, FALSE);
 
+  g_print ("gst_bin_get_by_name\n");
   mux = gst_bin_get_by_name (GST_BIN (pipeline), "mux");
   advertise_service (mux);
   gst_object_unref (mux);
 
   /* Put a bus handler */
+  g_print ("gst_pipeline_get_bus\n");
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
   gst_bus_add_signal_watch (bus);
   g_signal_connect (bus, "message", (GCallback) _on_bus_message, mainloop);
 
   /* Start pipeline */
+  g_print ("gst_element_set_state GST_STATE_PLAYING\n");
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  g_print ("g_main_loop_run\n");
   g_main_loop_run (mainloop);
 
+  g_print ("gst_element_set_state GST_STATE_NULL\n");
   gst_element_set_state (pipeline, GST_STATE_NULL);
 
   gst_object_unref (pipeline);
   gst_object_unref (bus);
 
+  g_print ("done\n");
   return 0;
 }
