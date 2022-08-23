6efd115b8ddd5d99dcdbcd3c594d8f18646af284
Dan Hodge <d.hodge@sechoia.com>
2022-08-23 15:52:54 +0100

    Mux to demux pipeline established

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #define PIPELINE_STR "videotestsrc num-buffers=1000 ! x264enc ! queue ! mpegtsmux name=mux ! fakesink"
 
 static void
-_on_bus_message (GstBus * bus, GstMessage * message, GMainLoop * mainloop)
+on_bus_message (GstBus * bus, GstMessage * message, GMainLoop * mainloop)
 {
   switch (GST_MESSAGE_TYPE (message)) {
     case GST_MESSAGE_ERROR:
@@ -18,46 +18,78 @@ _on_bus_message (GstBus * bus, GstMessage * message, GMainLoop * mainloop)
   }
 }
 
+static void on_pad_added(GstElement *element, GstPad *pad, gpointer data)
+{
+  GstPad *sinkpad;
+  GstElement *decoder = (GstElement *) data;
+
+  /* We can now link this pad with the its corresponding sink pad */
+  g_print("Dynamic pad created, linking demuxer/decoder\n");
+  sinkpad = gst_element_get_static_pad(decoder, "sink");
+  gst_pad_link(pad, sinkpad);
+  gst_object_unref(sinkpad);
+}
+
 int
 main (int argc, char **argv)
 {
+  // TODO DH check unrefs!
   g_print ("main\n");
   GstElement *pipeline = NULL;
-  GstElement *source, *convert, *sink;
-  GError *error = NULL;
+  GstElement *source, *convert1, *x264enc, *queue1, *mpegtsmux, *tsdemux;
+  GstElement *h264parse, *decoder, *convert2, *sink;
   GstBus *bus;
   GMainLoop *mainloop;
+  GstElementFactory *mpegtsmux_factory;
 
-  source = convert = sink = NULL;
+  source = convert1 = x264enc = queue1 = mpegtsmux = tsdemux = NULL;
+  h264parse = decoder = convert2 = sink = NULL;
 
   g_print ("gst_init\n");
   gst_init (&argc, &argv);
 
   g_print ("creating pipeline\n");
 
-  pipeline = gst_pipeline_new("video-file-metadata-pipeline");
+  mpegtsmux_factory = gst_element_factory_find ("mpegtsmux");
+  g_return_val_if_fail (mpegtsmux_factory != NULL, 1);
 
-  if (!pipeline) {
-    g_print ("pipeline could not be constructed\n");
-    return 1;
-  }
+  pipeline = gst_pipeline_new("video-file-metadata-pipeline");
+  g_return_val_if_fail (pipeline != NULL, 1);
 
   g_print ("adding elements\n");
 
-  source  = gst_element_factory_make("videotestsrc", "source");
-  convert = gst_element_factory_make("autovideoconvert", "convert");
-  sink    = gst_element_factory_make("xvimagesink", "sink");
+  source    = gst_element_factory_make("videotestsrc", "source");
+  convert1  = gst_element_factory_make("autovideoconvert", "convert1");
+  x264enc   = gst_element_factory_make("x264enc", "x264enc");
+  queue1    = gst_element_factory_make("queue", "queue1");
+  mpegtsmux = gst_element_factory_make("mpegtsmux", "mpegtsmux");
+  tsdemux   = gst_element_factory_make("tsdemux", "tsdemux");
 
-  if(!source || !convert || !sink) {
+  h264parse  = gst_element_factory_make("h264parse", "h264parse");
+  decoder = gst_element_factory_make("nvv4l2decoder", "decoder");
+  convert2   = gst_element_factory_make("videoconvert", "convert2");
+  sink       = gst_element_factory_make("nvoverlaysink", "sink");
+
+
+  if(!source || !convert1 || !x264enc || !queue1 || !mpegtsmux || !tsdemux || !sink) {
     g_print ("could not create elements\n");
     return 1;
   }
 
-  gst_bin_add_many(GST_BIN(pipeline), source, convert, sink, NULL);
-  if (gst_element_link_many(source, convert, sink, NULL) != TRUE) {
-    g_print ("could not link elements\n");
+  gst_bin_add_many(GST_BIN(pipeline), source, convert1, x264enc, queue1, mpegtsmux, tsdemux,
+                   h264parse, decoder, convert2, sink, NULL);
+
+  if (gst_element_link_many(source, convert1, x264enc, queue1, mpegtsmux, tsdemux, NULL) != TRUE) {
+    g_print ("could not link pre-demux elements\n");
     return 1;
   }
+
+  if (gst_element_link_many(h264parse, decoder, convert2, sink, NULL) != TRUE) {
+    g_print ("could not link pre-demux elements\n");
+    return 1;
+  }
+
+  g_signal_connect(tsdemux, "pad-added", G_CALLBACK(on_pad_added), h264parse);
 
   g_object_set(source,
                "num-buffers", 100,
@@ -70,7 +102,7 @@ main (int argc, char **argv)
   g_print ("gst_pipeline_get_bus\n");
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
   gst_bus_add_signal_watch (bus);
-  g_signal_connect (bus, "message", (GCallback) _on_bus_message, mainloop);
+  g_signal_connect (bus, "message", (GCallback) on_bus_message, mainloop);
 
   /* Start pipeline */
   g_print ("gst_element_set_state GST_STATE_PLAYING\n");
